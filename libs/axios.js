c82c1fdbc56c94ece7d10436f14042a8e9359ec7
Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>
2025-11-30 01:31:15 +0530

    change backend uri

--- libs/axios.js
+++ libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://judgingbackend.damrufest.org",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
